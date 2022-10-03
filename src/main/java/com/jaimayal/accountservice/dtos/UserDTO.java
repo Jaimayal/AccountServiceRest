38997bde2dfbd534c7fc2e7bc7d4e1ce4b9f4a31
Jaimayal <jaime.ayalaa.76@gmail.com>
2022-10-03 13:45:47 -0500

    Fixed UserDTO Validation

--- src/main/java/com/jaimayal/accountservice/dtos/UserDTO.java
+++ src/main/java/com/jaimayal/accountservice/dtos/UserDTO.java
@@ -16,22 +16,22 @@ public class UserDTO {
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private Long id;
     
-    @JsonProperty(value = "name", required = true)
+    @JsonProperty(value = "name")
     @NotBlank(message = "Name is required")
     private String name;
     
-    @JsonProperty(value = "lastName", required = true)
+    @JsonProperty(value = "lastName")
     @NotBlank(message = "lastName is required")
     private String lastName;
     
-    @JsonProperty(value = "email", required = true)
+    @JsonProperty(value = "email")
     @NotBlank(message = "email is required")
     @Pattern(regexp = "^[\\w\\.-]+@[a-zA-Z-]+(\\.[a-zA-Z]+)+$", 
             message = "email must contain only letters, " +
                     "one @ and one (or multiple) end domain(s) (.com.es, .us, .com, .arg.io, etc)")
     private String email;
     
-    @JsonProperty(value = "password", required = true, access = JsonProperty.Access.WRITE_ONLY)
+    @JsonProperty(value = "password", access = JsonProperty.Access.WRITE_ONLY)
     @ToString.Exclude
     @NotBlank(message = "password is required")
     @Size(min = 8, max = 30, message = "password must be within 8 and 30 chars long")
